f0d64d76c6c501687c8cafa416b60f2c73e1e969
Girinda <girindade@gmail.com>
2025-06-26 21:08:15 +0200

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,16 @@
+# tuple
+
+my_tuple = (1,2,3,4,5)
+
+print(my_tuple)
+print(my_tuple[0])
+print(my_tuple[2])
+print(my_tuple[-1])
+
+tuble1 = (1,2,3)
+tuble2 = (4,5,6)
+
+conc_tuple = tuple1 + tuple2
+rep_tuble = tuple*3
+
+print(rep_tuple)
