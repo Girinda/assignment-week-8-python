0ce362edd3e9b2a98e1b09e6b15750ebcf5a986c
Girinda <girindade@gmail.com>
2025-06-26 21:00:33 +0200

    Create lesson2.list.py

---
+++ lesson2.list.py
@@ -0,0 +1,19 @@
+
+fruit = ["apple","bananas","cherry"]
+
+fruits.append("kiwi")
+fruits(fruits)
+
+fruits.insert(1, corange")
+print(fruits)
+
+fruits.remove("kiwi")
+print(fruits)
+
+fruits.sort()
+print(fruits)
+
+or
+
+fruits.sort(reverse)
+print(fruits)
