0fbbb86103d8046d744b491b2d10da76c27f6134
Girinda <girindade@gmail.com>
2025-06-30 15:22:26 +0200

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,32 @@
-1
+#Operators
+
+# addition (+)
+# subtraction (-)
+# multiplication (×)
+# division (÷)
+# Modulus (%)
+# Exponents (**)
+
+x = 10
+y = 2
+
+print(x+y)
+print(x-y)
+print(x×y)
+print(x÷y)
+print(x%y)
+print(x*y)
+
+print(x**y)
+
+
+or
+
+x = 10
+x + = 2
+
+print(×)
+print(-)
+print(×)
+print(%)
+print(**)
